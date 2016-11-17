a4d9a2a79598968b43cb0a826589dee2a2ec3ba1
Joe Nash <joenash@github.com>
2016-11-17 17:34:56 +0000

    Part 1 template

--- src/App.hs
+++ src/App.hs
@@ -15,8 +15,41 @@ import           System.IO
 
 -- * api
 
+type API = "users" :> Get '[JSON] [User]
+
+-- * Users
+
+data User = User
+  { name :: String
+  , age :: Int
+  , email :: String
+  } deriving (Eq, Show, Generic)
+
+instance FromJSON User
+instance ToJSON User
+
+users1 :: [User]
+users1 =
+  [ User "Richard Davies" 14 "xXDarkHeart17Xx@aol.com"
+  , User "Ada Lovelace" 100 "og_ada@gmail.com"
+  ]
 
 -- * app
 
 run :: IO ()
-run = putStrLn "Hello, Nottingham!"
+run = do
+  let port = 3000
+      settings =
+        setPort port $
+        setBeforeMainLoop (hPutStrLn stderr ("listening on port " ++ show port)) $
+        defaultSettings
+  runSettings settings =<< app1
+
+server1 :: Server API
+server1 = return users1
+
+userAPI :: Proxy API
+userAPI = Proxy
+
+app1 :: IO Application
+app1 = return $ serve userAPI server1
