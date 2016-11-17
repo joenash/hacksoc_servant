d82b48edd450019337cd2be792ad583e654c9a61
Joe Nash <joenash@github.com>
2016-11-17 17:19:09 +0000

    Initial commit

---
+++ src/App.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeOperators #-}
+
+module App where
+
+import           Control.Monad.Trans.Except
+import           Data.Aeson
+import           GHC.Generics
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
+import           System.IO
+
+-- * api
+
+
+-- * app
+
+run :: IO ()
+run = putStrLn "Hello, Nottingham!"
